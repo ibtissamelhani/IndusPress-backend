77ccca6b0140d4ec6320a977bf65c98872da1120
ibtissam EL HANI <ibtissamelhani36@gmail.com>
2025-07-16 10:16:00 +0200

    first commit

---
+++ src/main/java/org/ibtissam/induspress/IndusPressApplication.java
@@ -0,0 +1,13 @@
+package org.ibtissam.induspress;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IndusPressApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IndusPressApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/ibtissam/induspress/IndusPressApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ibtissam.induspress;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IndusPressApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
